2b85a41ecfaf17bec171bc3dcec0f06206167704
Patrick Moody <patrick.n.moody@gmail.com>
2013-07-19 23:09:42 -0500

    able to post new item

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,3 +1,28 @@
 class ItemsController < ApplicationController
 
+	def index
+		@items = Item.all
+	end
+
+	def new
+		@item = Item.new
+	end
+
+	def create
+		p "============================="
+		p params[:item]
+		p current_user
+		if current_user
+			@item = current_user.items.create(item_params)
+		end
+		redirect_to root_path
+	end
+
+
+	private
+
+  def item_params
+    params.require(:item).permit(:description)
+  end
+
 end
