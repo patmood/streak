5f378d13b63759b8239a56f6fa3eb6ac5003a35c
patmood <patrick.n.moody@gmail.com>
2013-08-21 11:38:57 -0700

    model specs

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -3,18 +3,36 @@ require 'spec_helper'
 describe ItemsController do
 
   before(:each) do
-    @item_complete = Item.create(
-      :name
-      :uid
-      :provider
-      :globalstreak
-
-
+    @user = User.create(
+        name: "testuser",
+        uid: 123,
+        provider: "twitter",
+        globalstreak: 3
       )
-    @item_incomplete
+    @item_incomplete = @user.items.create(
+        description: "Do this",
+        donetoday: false,
+        localstreak: 5,
+        maxstreak: 10
+      )
+    @item_complete = @user.items.create(
+        description: "Done",
+        donetoday: true,
+        localstreak: 5,
+        maxstreak: 10
+      )
   end
 
   describe '#index' do
+    # context 'when user is logged in' do
+    #   before(:each) do
+    #     session[:user_id] = @user.id
+    #   end
+
+    #   get :index
+    #   pending
+
+    # end
     pending
   end
 
